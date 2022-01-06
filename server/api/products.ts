26430d52654ef915a39e18c54eaa677d92773b9c
Carlos Eli Lopez Tellez <30485301+Serveix@users.noreply.github.com>
2022-01-06 02:00:51 -0600

    Update products.ts

--- server/api/products.ts
+++ server/api/products.ts
@@ -1,9 +1,23 @@
 import { getFirestore } from 'firebase-admin/firestore'
+import { initializeApp, getApps, cert } from 'firebase-admin/app'
 
-export default async (req, res) => {
+const apps = getApps()
+
+if (!apps.length) {
+    initializeApp({
+        credential: cert('./online-store-nuxt-3-xxxxxxxxxxxx.json') // 👈 Path to your JSON Firebase certificate
+    })
+}
+
+export default async (request, response) => {
     const db = getFirestore()
     const productsSnap = await db.collection('products').get()
-    const productsData = productsSnap.docs.map(doc => doc.data())
+    const productsData = productsSnap.docs.map(doc => {
+        return {
+            uuid: doc.id,
+            ...doc.data()
+        }
+    })
     
     return productsData
 }
